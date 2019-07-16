1588a2989333cf7f8c37151d4aa57565e52f996d
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-07-16 12:15:37 -0700

    fixing for Travis

--- src/app.js
+++ src/app.js
@@ -4,23 +4,18 @@ const test = document.getElementById('test');
 const result = document.getElementById('result');
 
 test.onclick = function() {
-    name = '';
-    const firstname = prompt('What\'s your first name?');
     let correctAnswers = 0;
     const answer1 = prompt('The green sturgeon reaches maturity at around 12 years of age (Y/N)');
     if(!isYes(answer1)) {
         correctAnswers += 1;
-        console.log(correctAnswers);
-    };
+    }
     const answer2 = prompt('Unlike other marine mammals, sea otters have no layer of blubber (Y/N)');
     if(isYes(answer2)) {
         correctAnswers += 1;
-        console.log(correctAnswers);
-    };
+    }
     const answer3 = prompt('All salmon, including coho, are anadromous, meaning they spend part of their lives in the ocean and part in freshwater (Y/N)'); 
     if(isYes(answer3)) {
         correctAnswers += 1;
-        console.log(correctAnswers);
     }
     result.textContent = `You got ${Math.floor((correctAnswers/3) * 100)}% test score!`;
 };
