5033d62a0bb7f9c421f58e6ac3b4b175410ee57d
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-07-20 13:52:35 -0700

    Final edits, want to add more refactoring

--- src/app.js
+++ src/app.js
@@ -1,31 +1,33 @@
 let guessCounter = 3;
-const message = document.getElementById('message');
-const actualNumber = Math.floor(Math.random() * Math.floor(20));
 let guessValue = 0;
+const message = document.getElementById('message');
 const guessMore = document.getElementById('guessmore');
 const submitNumber = document.getElementById('submitnumber');
 const guessLeft = document.getElementById('guessesleft');
+const actualNumber = Math.floor(Math.random() * 21)
 
 submitNumber.addEventListener('click', function() {
+    console.log(actualNumber);
     const numberInput = document.getElementById('numberinput');
     const number = numberInput.value;
-    if(number > actualNumber) {
+    if(number < 0 || number > 20) {
+        message.textContent = 'Please provide a valid number';
+        return;
+    } else if(number > actualNumber) {
+        guessCounter--;
         guessValue++;
-        guessCounter--;
     } else if(number < actualNumber) {
-        guessValue--;
         guessCounter--;
+        guessValue--;
     } else {
         guessValue = 0;
+        guessCounter--;
     }
-// Creaeted a condition checker that increments and decrememnts a variable based on whether or not that number is too high or too low, or just right.
-// It also removes one from the guess counter with each guess, which starts with a value of 3
 
-// This will disable the input field if the user runs out of guesses, and say you lose, if they still have guesses, it will tell them to keep guessing.
     if(guessCounter === 0) {
         numberInput.disabled = true;
         message.textContent = 'You Lose';
-        guessLeft.textContent = `You have ${guessCounter} guesses left`
+        guessLeft.textContent = `You have ${guessCounter} guesses left`;
     } else if(guessCounter === 1) {
         message.textContent = 'Keep Guessing';
         guessLeft.textContent = `You have ${guessCounter} guess left`;
@@ -33,19 +35,23 @@ submitNumber.addEventListener('click', function() {
         message.textContent = 'Keep Guessing';
         guessLeft.textContent = `You have ${guessCounter} guesses left`;
     }
-
-// Based off of the guessValue variable, which is assigned a value based off of whether or not the user's number was too high, too low, or just right, we create
-// Conditions for what happens when that number has a certain value.
-    if(guessValue >= 1) {
-        guessMore.textContent = 'Too High';
-        guessValue = 0; 
-        return;
-    } else if(guessValue < 0) {
+    
+    if(guessValue < 0) {
         guessMore.textContent = 'Too Low';
         guessValue = 0;
         return;
-    } else {
+    } else if(guessValue === 1) {
+        guessMore.textContent = 'Too High';
+        guessValue = 0;
+        return;
+    } else if(guessValue === 0){
+        guessMore.textContent = 'Correct';
         message.textContent = 'You Won!';
         numberInput.disabled = true;
+        submitNumber.disabled = true;
     }
 });
+
+    
+
+
