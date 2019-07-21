aaff6a8b31239af9aba26c7e8c12c37c23486f2a
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-07-20 19:47:19 -0700

    mostly finished product, few more changes to be commiteed

--- src/app.js
+++ src/app.js
@@ -9,45 +9,76 @@ const tiesCount = document.getElementById('ties-count');
 const computerImage = document.getElementById('computer-choice');
 const addMessage = document.getElementById('message');
 
+let moneyRemaining = 50;
+let updateMoney = document.getElementById('moneyremaining');
+let amount = document.getElementById('amount');
+let currentBet = 0;
+let bet = document.getElementById('bet');
+updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+
 let wins = 0;
 let losses = 0;
 let ties = 0;
 
 let message = '';
 // throw rock, paper, or scissors, based off random number. Function onclick of a random number.
+bet.addEventListener('click', () => {
+    if(moneyRemaining >= 0) {
+        currentBet = Number(amount.value);
+    } else {
+        updateMoney.textContent = 'YOU CAN\'T BET ANYMORE MONEY, GO HOME!';
+        bet.disabled = true;
+        amount.disabled = true;
+    }
+});
 
 throwButton.addEventListener('click', () => {
     const computerChoice = getThrow();
     const userChoice = document.querySelector('input:checked').value;
 
-    if(userChoice === 'rock' && computerChoice === 'scissors') {
-        message = 'YOU WON!';
+    if(userChoice === 'rock' && computerChoice === 'scissors' && moneyRemaining > 0) {
+        message = `YOU WON ${currentBet}!`;
         wins++;
         winCount.textContent = wins;
-    } else if(userChoice === 'rock' && computerChoice === 'paper') {
-        message = 'YOU LOST!';
+        moneyRemaining += currentBet;
+        updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+    } else if(userChoice === 'rock' && computerChoice === 'paper' && moneyRemaining > 0) {
+        message = `YOU LOST ${currentBet}!`;
         losses++;
         lossCount.textContent = losses;
-    } else if(userChoice === 'paper' && computerChoice === 'rock') {
-        message = 'YOU WON!';
+        moneyRemaining -= currentBet;
+        updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+    } else if(userChoice === 'paper' && computerChoice === 'rock' && moneyRemaining > 0) {
+        message = `YOU WON ${currentBet}!`;
         wins++;
         winCount.textContent = wins;
-    } else if(userChoice === 'paper' && computerChoice === 'scissors') {
-        message = 'YOU LOST!';
+        moneyRemaining += currentBet;
+        updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+    } else if(userChoice === 'paper' && computerChoice === 'scissors' && moneyRemaining > 0) {
+        message = `YOU LOST ${currentBet}!`;
         losses++;
         lossCount.textContent = losses;
-    } else if(userChoice === 'scissors' && computerChoice === 'rock') {
-        message = 'YOU LOST!';
+        moneyRemaining -= currentBet;
+        updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+    } else if(userChoice === 'scissors' && computerChoice === 'rock' && moneyRemaining > 0) {
+        message = `YOU LOST ${currentBet}!`;
         losses++;
         lossCount.textContent = losses;
-    } else if(userChoice === 'scissors' && computerChoice === 'paper') {
-        message = 'YOU WON!';
+        moneyRemaining -= currentBet;
+        updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+    } else if(userChoice === 'scissors' && computerChoice === 'paper' && moneyRemaining > 0) {
+        message = `YOU WON ${currentBet}!`;
         wins++;
         winCount.textContent = wins;
-    } else {
+        moneyRemaining += currentBet;
+        updateMoney.textContent = `YOU HAVE $${moneyRemaining} TO BET`;
+    } else if(userChoice === computerChoice && moneyRemaining > 0){
         message = 'IT WAS A TIE!';
         ties++;
         tiesCount.textContent = ties;
+    } else {
+        throwButton.disabled = true;
+        message = 'YOU CAN\'T BET ANYMORE MONEY, GO HOME!';
     }
     computerImage.classList.remove('invisible');
     const src = 'assets/images/' + computerChoice + '.jpg';
