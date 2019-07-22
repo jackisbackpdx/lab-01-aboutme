fa7ab3eaed697b34d113418472ef481e0d3eec26
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-07-22 11:37:00 -0700

    fixed src inconsistency

--- test/test2.js
+++ test/test2.js
@@ -1,2 +1,19 @@
-import './test-function.js';
-import './tests.js';
+import testFunction from './test-function.js';
+const test = QUnit.test;
+
+
+test('too high or too low?', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const test = 12;
+    const actualNumber = 10;
+    const expected = 1;
+      
+    //Act
+    const result = testFunction(test, actualNumber);
+    // Call the function you're testing and set the result to a const
+    
+    //Assert
+    assert.equal(result, expected);
+});
+
